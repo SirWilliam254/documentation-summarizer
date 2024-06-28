fc7aba038c8b4717b680ba06e3ea067d39657624
William Mburu <williammburu79@gmail.com>
2024-06-28 16:12:45 +0300

    modified:   index.html 	modified:   src/App.jsx 	new file:   src/components/Demo.jsx 	new file:   src/components/Hero.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+// Imports
+import Hero from "./components/Hero";
+import Demo from "./components/Demo";
+
+import './App.css';
 
 const App = () => {
   return (
-    <div>
-      Application
-    </div>
+    <main>
+        <div className="main">
+            <div className="gradient" />
+        </div>
+        <div className="app">
+            <Hero />
+            <Demo />
+        </div>
+    </main>
   )
 }
 
