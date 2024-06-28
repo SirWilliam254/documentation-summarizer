45b7dc22b28b9e8a8956671a47809e10d0d54820
William Mburu <williammburu79@gmail.com>
2024-06-28 17:54:58 +0300

    new file:   src/assets/chip.png 	modified:   src/assets/index.js 	modified:   src/components/Demo.jsx 	modified:   src/components/Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import { logo } from '../assets';
 
 const Hero = () => {
   return (
-    <div>
-      Hero section
-    </div>
+    <header className="w-full flex justify-center items-center flex-col">
+    <nav className="flex justify-between items-center w-full mb-10 pt-3 px-2"> 
+        <img src={logo} alt="sumz_logo" className="w-14 object-contain" />
+
+        <button
+        type='button'
+        onClick={() => window.open('https://github.com/SirWilliam254/ai-summarizer')}
+        className='black_btn'
+        >
+            Github
+        </button>
+    </nav>
+    <h1 className='head_text'>Summarize articles with<span className='orange_gradient'> Open AI GPT-4
+    </span></h1>
+    <h2 className='desc'>
+        summarize lengthy articles to more succinct form using chat gpt
+    </h2>
+    </header>
   )
 }
 
